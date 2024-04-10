c5a25b0bef3b74ac80ae8222e3d7cfce5398cc19
BIcaza <166575035+BIcaza@users.noreply.github.com>
2024-04-09 21:09:49 -0500

    Add files via upload

---
+++ taller/taller.js
@@ -0,0 +1,120 @@
+//1. Declara una variable llamada nombre y asígnale tu nombre como valor.
+
+let nombre = "Billy";
+
+//2. Crea una variable llamada edad y asígnale tu edad.
+let edad = "19";
+
+//3.Declara una variable ‘esEstudiante’ y asígnale un valor booleano.
+
+if (esEstudiante) {
+    console.log("La persona es estudiante.");
+} else {
+    console.log("La persona no es estudiante.");
+}
+
+//4.Crea una variable llamada puntaje y no le asignes ningún valor inicial.
+
+let puntaje = "";
+
+//5. Declara una variable hobbies que contenga un array con tus hobbies.
+
+let hobbies = ["Dibujar","leer","pintar"];
+
+//6. Crea un objeto persona que contenga tres propiedades: nombre, edad y esEstudiante.
+
+let persona = ["Billy", "19" ,"Esestudiante"];
+let Nombre = "Billy"
+let Edad = "19"
+let Esestudiante = true
+//7. Declara una variable nullVar y asígnale el valor null.
+
+let  nullVar = null;
+
+//8. Crea una variable indefinida sin asignarle un valor.
+ 
+let a =""
+
+//9. Declara una variable numeroHex que contenga un número hexadecimal.
+
+let numerohex = "0xf"
+
+//10. Crea una variable nombreCompleto que concatene tu nombre y tu apellido.
+
+let NOmbre = "Billy";
+let apellido = "Icaza";
+let nombreCompleto = NOmbre + " " + apellido;
+console.log(nombreCompleto);
+
+//11. Suma dos números y almacena el resultado en una variable suma.
+let num1 = 5;
+let num2 = 5;
+let suma = num1 + num2;
+console.log (suma);
+
+//12. Resta dos números y guarda el resultado en una variable resta.
+let Num1 = 10;
+let Num2 = 5;
+let resta = Num1 - Num2;
+console.log(resta);
+
+//13. Multiplica dos números y asigna el resultado a una variable multiplicacion.
+
+let numero1 = 10;
+let numero2 = 5;
+let multiplicacion = numero1 * numero2;
+console.log(multiplicacion); 
+
+//14. Divide dos números y almacena el resultado en una variable division.
+
+let NUMERO1 = 10;
+let NUMERO2 = 5;
+let division = NUMERO1 / NUMERO2;
+console.log(division);
+
+//15. Usa el operador de módulo para encontrar el residuo de 10 dividido por 3 y almacena el resultado en una variable modulo.
+
+let modulo = 10 % 3;
+console.log(modulo);
+
+//16. Incrementa el valor de una variable contador en 1 usando el operador de incremento.
+let contador = 0
+contador = ++
+console.log (contador)
+
+//17. Decrementa el valor de una variable contadorDec en 1 usando el operador de decremento.
+let contadordec = 2;
+contadordec--; 
+console.log(contadordec);
+
+//18. Compara dos números para ver si son iguales y almacena el resultado (booleano) en una variable sonIguales.
+
+let NUMErO1 = 10;
+let NUMErO2 = 5;
+let sonIguales = NUMErO1 === NUMErO2;
+
+if (sonIguales) {
+    console.log("Los números son iguales.");
+} else {
+    console.log("Los números no son iguales.");
+}
+
+
+//19. Comprueba si un número es mayor que otro y guarda el resultado (booleano) en una variable esMayor.
+
+let numEro1 = 10;
+let numEro2 = 5;
+let esMayor = numEro1 > numEro2;
+
+if (esMayor) {
+    console.log("El primer número es mayor que el segundo.");
+} else {
+    console.log("El primer número no es mayor que el segundo.");
+}
+
+
+//20. Utiliza el operador de asignación compuesta para sumar y asignar un valor a una variable.
+
+let variable = 5;
+variable += 3; 
+console.log(variable);
